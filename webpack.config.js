cc448092df6f246d3d9292e17c8de7a1db429796
VirsatusCodes <donavynpence@yahoo.com>
2022-06-26 21:44:23 -0700

    removed index.html resetting every load

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -10,15 +9,9 @@ module.exports = {
       devServer: {
         static: './dist',
       },
-      plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Output Management',
-    }),
-  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
   },
   optimization: {
     runtimeChunk: 'single',
