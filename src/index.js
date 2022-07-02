8b6c65407e4b2563997d4d8db53340ffb8e72a39
VirsatusCodes <donavynpence@yahoo.com>
2022-07-02 11:14:11 -0700

    plan of action complete, files created and linked.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './style.css'
+import * as daily from './modules/module-daily';
+import * as image from './modules/module-image';
+import * as projects from './modules/module-projects';
 
 console.log("tessssst");
