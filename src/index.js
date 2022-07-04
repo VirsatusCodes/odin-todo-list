4988618f8b3960c9157d831119669035d1936346
VirsatusCodes <donavynpence@yahoo.com>
2022-07-04 14:07:58 -0700

    wideDom F, creation and render of daily-tasks done need to include user adding tasks next and wire buttons for adding tasks and removing tasks.

--- src/index.js
+++ src/index.js
@@ -5,4 +5,20 @@ import * as projects from './modules/module-projects';
 
 console.log("tessssst");
 
+function wipeDOM(){
+    const mainInfo = document.querySelector('.main-info');
+    while(mainInfo.firstChild){
+        mainInfo.removeChild(mainInfo.firstChild);
+    }
+}
+
 image.imgRender();
+
+const dailyTasksButton = document.querySelector('.daily-tasks');
+
+dailyTasksButton.addEventListener('click', () => {
+    wipeDOM()
+    daily.renderUI();
+    daily.dailyTasksLibrary.forEach(daily.renderTasks);
+})
+
--- src/modules/module-daily.js
+++ src/modules/module-daily.js
@@ -1,8 +1,45 @@
-export{}
-console.log('test 1')
+export{renderUI, renderTasks, dailyTasksLibrary}
 
-const dailyTasks = document.querySelector('.daily-tasks');
+const mainInfo = document.querySelector('.main-info');
+const dailyTasksLibrary = [];
 
-dailyTasks.addEventListener('click', () => {
-    console.log('far')
-})
+const createTasks = (taskName, taskTime) => {
+    return {taskName, taskTime}
+};
+
+const task1 = createTasks('Wake Up!','8:00');
+dailyTasksLibrary.push(task1);
+
+function renderUI(){
+    const UIContainer = document.createElement('div');
+    UIContainer.classList.add('box');
+    mainInfo.appendChild(UIContainer);
+
+    const h3 = document.createElement('h2');
+    h3.textContent = 'Add a Task! :)';
+    UIContainer.appendChild(h3);
+
+    const addTaskButton = document.createElement('button');
+    addTaskButton.classList.add('add-task');
+    addTaskButton.textContent = '+';
+    UIContainer.appendChild(addTaskButton);
+};
+
+function renderTasks(task){
+    const taskContainer = document.createElement('div');
+    taskContainer.classList.add('box');
+    mainInfo.appendChild(taskContainer);
+
+    const text = document.createElement('p');
+    text.textContent = task.taskName;
+    taskContainer.appendChild(text);
+
+    const time = document.createElement('p');
+    time.textContent = task.taskTime;
+    taskContainer.appendChild(time);
+
+    const removeTaskButton = document.createElement('button');
+    removeTaskButton.classList.add('remove-task');
+    removeTaskButton.textContent = '-';
+    taskContainer.appendChild(removeTaskButton);
+};
